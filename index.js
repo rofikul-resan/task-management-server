2f81cac980d0b958f31646a53c3e1113490360ff
rofikul-resan <resan6231@gmail.com>
2023-06-14 23:06:18 +0600

    add task post api

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("task management server is running");
+});
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.absippg.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const taskCollocation = client.db("task-management").collection("task");
+    app.post("/add-task", async (req, res) => {
+      const data = req.body;
+      const userTask = await taskCollocation
+        .find({ name: data?.name })
+        .toArray();
+      if (userTask) {
+        const isExistTask = userTask.filter(
+          (task) => task.title === data.title
+        );
+        if (isExistTask) {
+          return res.send({ message: "This task already Exist " });
+        }
+      }
+      const result = await taskCollocation.insertOne(data);
+      res.send(result);
+      console.log(data);
+    });
+    // Send a ping to confirm a successful connection
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+app.listen(port);
