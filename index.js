d69911a23a357971c4d336aa103cf04cdfda2ecf
rofikul-resan <resan6231@gmail.com>
2023-06-15 10:20:11 +0600

    add delete motehde

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("task management server is running");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.absippg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -51,6 +51,12 @@ async function run() {
         }
       }
       const result = await taskCollocation.insertOne(data);
+      res.send(result);
+    });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await taskCollocation.deleteOne({ _id: new ObjectId(id) });
       res.send(result);
     });
     // Send a ping to confirm a successful connection
